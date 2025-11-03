93c755879349a8a43efd4a08fc09bb80f7767e23
haktanelitez <haktan.elitez@std.yeditepe.edu.tr>
2025-11-03 07:18:38 +0300

    assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -7,9 +7,9 @@ def find_period(L0,L1):
     
     for L in range(L0, L1 + 1):
         T = 2 * np.pi * np.sqrt(L / g)
-        print(f"When L = {float(L)} m, T = {T:.1f} s")
+        print(f"When L  = {float(L)} m, T = {T:.1f} s")
         
-    return T1, T0
+    return T0, T1
 
     
     
